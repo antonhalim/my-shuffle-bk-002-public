c28ce3c82f8c7886fa2fdbe0fa5a7657a6202f7f
axton21 <axton_n@yahoo.com>
2015-03-25 09:34:41 -0400

    done

--- lib/my_shuffle.rb
+++ lib/my_shuffle.rb
@@ -1,1 +1,17 @@
-# your code goes here
+class Array
+
+	def my_shuffle
+		result = []
+		count = self.length
+		until result.length == count
+			unit = self.sample
+			if !result.include?(unit) && result.length != self.index(unit)
+				result << unit
+			elsif count == 1
+				return self
+			end
+		end
+		result
+	end
+
+end
